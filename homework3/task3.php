43262a10e392066fb4058f93bf5b439ae4df9420
Andrew <andrewinapple@gmail.com>
2018-10-31 18:22:48 +0300

    +

--- homework3/task3.php
+++ homework3/task3.php
@@ -14,10 +14,13 @@ $array = [
 ];
 
 foreach ($array as $index => $value) {
-    echo "<br>$index:<br>";
+    /*echo "<br>$index:<br>";
     foreach ($value as $key => $box) {
         echo "$box, ";
-    }
+    }*/
+    echo "$index:<br>";
+    echo implode(', ', $value) . '<br>';
+
 }
 
 ?>
