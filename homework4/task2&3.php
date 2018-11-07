908abb0a4025085e9d91648f8901a15249e37e50
Andrew <andrewinapple@gmail.com>
2018-11-07 18:04:52 +0300

    +

--- homework4/task2&3.php
+++ homework4/task2&3.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    //Файл стилей для плагина JQuery
+    <!--Файл стилей для плагина JQuery-->
     <link rel="stylesheet" href="dimsemenov-Magnific-Popup-2ff1692/dist/magnific-popup.css">
     <style>
         .gallery-image {
